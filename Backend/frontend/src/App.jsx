87ec08d2050fa5878350652ee779e99f796b62b0
Rohan Singh <rohansingh6393@gmail.com>
2025-08-19 14:53:42 +0530

    changed  node version for railway

--- Backend/frontend/src/App.jsx
+++ Backend/frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import MoodDetector from './components/MoodDetector'
 import MoodSongs from './components/MoodSongs'
